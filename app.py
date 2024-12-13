5525063dd71e9a2116e7718fce388f4f61e69121
Andrew Guilas <AndrewKyleGuilas@gmail.com>
2024-12-12 22:24:43 -0500

    Converted login scene to gui

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+import tkinter
 from gui.login_scene import LoginScene
-from gui.course_list_scene import CourseListScene
 
 def __main__():
-    current_user = LoginScene().show()
-    CourseListScene().show()
+    root = tkinter.Tk()
+    root.geometry('1200x720')
+    
+    login_scene = LoginScene()
+    login_scene.show(root)
+
+    root.mainloop()
 
 if __name__ == '__main__':
     __main__()
-    
--- gui/login_scene.py
+++ gui/login_scene.py
@@ -1,43 +1,86 @@
+import tkinter
+from gui.course_list_scene import CourseListScene
 from managers.user_manager import UserManager
 from models.user import User
 
+def strip_string(string):
+    return string.lower().strip().replace(" ", "")
+
 class LoginScene:
-    def show(self):
-        user_manager = UserManager()
-        user_manager.add_user(User("admin", "pass"))
+    def __init__(self):
+        self.user_manager = UserManager()
+        self.user_manager.add_user(User("admin", "pass")) # temp
 
-        while True:
-            print("\n\nLogin")
-            
-            username_input = input("Enter username: ")
-            password_input = input("Enter password: ")
+    def log_in(self):
+        username_input = self.username_field.get()
+        password_input = self.password_field.get()
 
-            try:
-                option_input = int(input("1. Login\n2. Register\n3. Exit\nEnter option: "))
-            except ValueError:
-                print("Invalid input")
-                continue
+        if not strip_string(username_input):
+            self.set_status("Error: Username cannot be blank")
+            return
+        
+        if not strip_string(password_input):
+            self.set_status("Error: Password cannot be blank")
+            return
 
-            match option_input:
-                case 1:
-                    user = user_manager.get_user(username_input)
-                    if not user:
-                        print("User does not exist")
-                        continue
+        user = self.user_manager.get_user(username_input)
+        if not user:
+            self.set_status("Error: User does not exist")
+            return
 
-                    if not user.authenticate(password_input):
-                        print("Incorrect password")
-                        continue
-                    
-                    print("Logged in")
-                    return user
-                case 2:
-                    new_user = User(username_input, password_input)
-                    user_manager.add_user(new_user)
-                    print("User created")
-                case 3:
-                    pass #TODO
-                case _:
-                    print("Invalid input")
-                    continue
-                
+        if not user.authenticate(password_input):
+            self.set_status("Error: Incorrect password")
+            return
+        
+        CourseListScene().show()
+
+    def register(self):
+        username_input = self.username_field.get()
+        password_input = self.password_field.get()
+
+        if not strip_string(username_input):
+            self.set_status("Error: Username cannot be blank")
+            return
+        
+        if not strip_string(password_input):
+            self.set_status("Error: Password cannot be blank")
+            return
+
+        username_input.lower()
+
+        if self.user_manager.get_user(username_input):
+            self.set_status("Error: Username already used")
+            return
+
+        new_user = User(username_input, password_input)
+        self.user_manager.add_user(new_user)
+
+        self.set_status("Success: User created")
+
+    def show(self, root):
+        title = tkinter.Label(root, text="Login")
+        title.grid(row=0)
+
+        username_text = tkinter.Label(root, text="Username")
+        username_text.grid(row=1)
+
+        self.username_field = tkinter.Entry(root)
+        self.username_field.grid(row=1, column=1)
+
+        password_text = tkinter.Label(root, text="Password")
+        password_text.grid(row=2)
+
+        self.password_field = tkinter.Entry(root, show="*")
+        self.password_field.grid(row=2, column=1)
+
+        login_button = tkinter.Button(root, text="Log In", command=self.log_in)
+        login_button.grid(row=3)
+
+        register_button = tkinter.Button(root, text="Register", command=self.register)
+        register_button.grid(row=4)
+
+        self.status_text = tkinter.Label(root)
+        self.status_text.grid(row=5)
+
+    def set_status(self, text):
+        self.status_text.config(text=text)
