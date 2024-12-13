5ddc853962dfbf26d82206174c18dbb04f84017a
Andrew Guilas <AndrewKyleGuilas@gmail.com>
2024-12-12 23:06:52 -0500

    Added search bar

--- gui/course_list_scene.py
+++ gui/course_list_scene.py
@@ -15,41 +15,18 @@ class CourseListScene:
         self.root = root
         self.frame = tkinter.Frame(root)
 
-        title = tkinter.Label(self.frame, text="Courses")
-        title.grid(row=0)
+        search_query = tkinter.StringVar()
+        search_query.trace_add("write", lambda name, index, mode, sv=search_query: self.filter_courses(search_query.get()))
+
+        self.search_field = tkinter.Entry(self.frame, textvariable=search_query)
+        self.search_field.grid(row=0)
+        self.search_field.focus()
 
         self.listbox = tkinter.Listbox(self.frame)
         self.listbox.grid(row=1)
 
         self.filter_courses("")
-        self.show_courses()
-
         self.frame.pack()
-
-        """
-        while True:
-            print("\n\nCourse List")
-            self.show_courses()
-            try:
-                option_input = int(input("1. Search\n2. Add Course\n3. My Reviews\n4. Log Out\nEnter option: "))
-            except ValueError:
-                print("Invalid input")
-                continue
-
-            match option_input:
-                case 1:
-                    search_query = input("Enter keyword: ")
-                    self.filter_courses(search_query)
-                case 2:
-                    pass #TODO
-                case 3:
-                    pass #TODO
-                case 4:
-                    pass #TODO
-                case _:
-                    print("Invalid input")
-                    continue
-        """
 
     def hide(self):
         for element in self.root.winfo_children():
@@ -57,20 +34,22 @@ class CourseListScene:
 
     def filter_courses(self, search_query):
         search_query = strip_string(search_query)
+        print(search_query)
 
         if search_query == "":
             self.displayed_courses = self.course_manager.get_courses()
-            return
+        else:
+            self.displayed_courses = []
+            for course in self.course_manager.get_courses():
+                if (search_query in strip_string(str(course))):
+                    self.displayed_courses.append(course)
 
-        self.displayed_courses = []
-        for course in self.course_manager.get_courses():
-            if (search_query in strip_string(str(course))):
-                self.displayed_courses.append(course)
+        self.show_courses()
 
     def show_courses(self):
+        self.listbox.delete(0, "end")
         for course in self.displayed_courses:
-            print(course)
-            self.listbox.insert(tkinter.END, course)
+            self.listbox.insert("end", course)
 
 def strip_string(string):
     return string.lower().strip().replace(" ", "")
